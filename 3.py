a50df120053b746abf9fab29e72e61912bd5bb6c
Paras Kapoor <paras@Parass-MacBook-Air.local>
2026-03-01 16:27:43 +0530

    Second commit using deepseek model

--- 3.py
+++ 3.py
@@ -6,13 +6,6 @@ OLLAMA_BASE_URL = "http://localhost:11434/v1"
 ollama = OpenAI(base_url=OLLAMA_BASE_URL, api_key="ollama")
 
 
-# Model response using llama3 model with a simple prompt.
-# response = ollama.chat.completions.create(
-#     model="llama3", messages=[
-#         {"role": "user", "content": "Tell me a fun fact."}])
-
-# print(response.choices[0].message.content)
-
 # Model response using deepseek model with a simple prompt.
 response = ollama.chat.completions.create(
     model="deepseek-r1:1.5b", messages=[
